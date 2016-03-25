80dfe2213105c449959cb8897ab51f48c12cd8c2
司华印 <sihuayin@jiandan100.cn>
2016-03-25 23:23:02 +0800

    标识

--- js/Map.js
+++ js/Map.js
@@ -15,8 +15,15 @@ Map.prototype.display = function() {
   this.map = this.game.add.tilemap("map1");
   this.map.addTilesetImage('tile');
   var layer1 = this.map.layer;
-  //创建新的层，使用远层的大小
-  this.layer = this.map.createLayer('bg1', layer1.heightInPixels, layer1.heightInPixels);
-  this.layer.position.x = this.worldWidth / 8;
-  this.layer.fixedToCamera = false;
+
+  var bmd = game.add.bitmapData(layer1.widthInPixels, layer1.heightInPixels);
+  bmd.context.fillStyle = '#ff0000';
+  bmd.context.fillRect(0, 0, layer1.widthInPixels, layer1.heightInPixels);
+
+  // var aScale = this.game.world.height / layer1.heightInPixels;
+  // //创建新的层，使用远层的大小
+  // this.layer = this.map.createLayer('bg1', layer1.heightInPixels, layer1.heightInPixels);
+  // this.layer.position.x = this.worldWidth / 8;
+  // this.layer.scale = {x: aScale, y: aScale};
+  // this.layer.fixedToCamera = false;
 }
